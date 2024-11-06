f8c1b9e9b7e32a1e5584a50b4138daebda20535c
Raffaele Vitale <150654623+raffaelevitale@users.noreply.github.com>
2024-11-06 15:46:10 +0100

    Creazione delle funzioni
    Creazione del contatto, modifica, ricerca e eliminazione di esso. Mancante l'inserimento della foto

--- Esercizio03 - Rubrica/Form1.Designer.cs
+++ Esercizio03 - Rubrica/Form1.Designer.cs
@@ -28,20 +28,240 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.modificaContattoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminaContattoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ricercaContattoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.esciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grbContatto = new System.Windows.Forms.GroupBox();
+            this.btnCreaContatto = new System.Windows.Forms.Button();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtIndirizzo = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCognome = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxContatti = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.grbContatto.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.modificaContattoToolStripMenuItem,
+            this.eliminaContattoToolStripMenuItem,
+            this.ricercaContattoToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.esciToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(626, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // modificaContattoToolStripMenuItem
+            // 
+            this.modificaContattoToolStripMenuItem.Name = "modificaContattoToolStripMenuItem";
+            this.modificaContattoToolStripMenuItem.Size = new System.Drawing.Size(116, 20);
+            this.modificaContattoToolStripMenuItem.Text = "Modifica Contatto";
+            this.modificaContattoToolStripMenuItem.Click += new System.EventHandler(this.modificaContattoToolStripMenuItem_Click);
+            // 
+            // eliminaContattoToolStripMenuItem
+            // 
+            this.eliminaContattoToolStripMenuItem.Name = "eliminaContattoToolStripMenuItem";
+            this.eliminaContattoToolStripMenuItem.Size = new System.Drawing.Size(108, 20);
+            this.eliminaContattoToolStripMenuItem.Text = "Elimina Contatto";
+            this.eliminaContattoToolStripMenuItem.Click += new System.EventHandler(this.eliminaContattoToolStripMenuItem_Click);
+            // 
+            // ricercaContattoToolStripMenuItem
+            // 
+            this.ricercaContattoToolStripMenuItem.Name = "ricercaContattoToolStripMenuItem";
+            this.ricercaContattoToolStripMenuItem.Size = new System.Drawing.Size(107, 20);
+            this.ricercaContattoToolStripMenuItem.Text = "Ricerca Contatto";
+            this.ricercaContattoToolStripMenuItem.Click += new System.EventHandler(this.ricercaContattoToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(24, 20);
+            this.toolStripMenuItem1.Text = "?";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // esciToolStripMenuItem
+            // 
+            this.esciToolStripMenuItem.Name = "esciToolStripMenuItem";
+            this.esciToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.esciToolStripMenuItem.Text = "Esci";
+            this.esciToolStripMenuItem.Click += new System.EventHandler(this.esciToolStripMenuItem_Click);
+            // 
+            // grbContatto
+            // 
+            this.grbContatto.Controls.Add(this.btnCreaContatto);
+            this.grbContatto.Controls.Add(this.txtTelefono);
+            this.grbContatto.Controls.Add(this.txtEmail);
+            this.grbContatto.Controls.Add(this.txtIndirizzo);
+            this.grbContatto.Controls.Add(this.txtNome);
+            this.grbContatto.Controls.Add(this.txtCognome);
+            this.grbContatto.Controls.Add(this.label5);
+            this.grbContatto.Controls.Add(this.label4);
+            this.grbContatto.Controls.Add(this.label3);
+            this.grbContatto.Controls.Add(this.label2);
+            this.grbContatto.Controls.Add(this.label1);
+            this.grbContatto.Location = new System.Drawing.Point(13, 28);
+            this.grbContatto.Name = "grbContatto";
+            this.grbContatto.Size = new System.Drawing.Size(269, 211);
+            this.grbContatto.TabIndex = 1;
+            this.grbContatto.TabStop = false;
+            this.grbContatto.Text = "Dati del contatto";
+            // 
+            // btnCreaContatto
+            // 
+            this.btnCreaContatto.Location = new System.Drawing.Point(11, 148);
+            this.btnCreaContatto.Name = "btnCreaContatto";
+            this.btnCreaContatto.Size = new System.Drawing.Size(173, 40);
+            this.btnCreaContatto.TabIndex = 10;
+            this.btnCreaContatto.Text = "Salva il Contatto";
+            this.btnCreaContatto.UseVisualStyleBackColor = true;
+            this.btnCreaContatto.Click += new System.EventHandler(this.btnCreaContatto_Click_1);
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(84, 122);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefono.TabIndex = 9;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(84, 94);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // txtIndirizzo
+            // 
+            this.txtIndirizzo.Location = new System.Drawing.Point(84, 68);
+            this.txtIndirizzo.Name = "txtIndirizzo";
+            this.txtIndirizzo.Size = new System.Drawing.Size(100, 20);
+            this.txtIndirizzo.TabIndex = 7;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(84, 42);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 6;
+            // 
+            // txtCognome
+            // 
+            this.txtCognome.Location = new System.Drawing.Point(84, 13);
+            this.txtCognome.Name = "txtCognome";
+            this.txtCognome.Size = new System.Drawing.Size(100, 20);
+            this.txtCognome.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Telefono";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "E-Mail";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Indirizzo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cognome";
+            // 
+            // listBoxContatti
+            // 
+            this.listBoxContatti.FormattingEnabled = true;
+            this.listBoxContatti.Location = new System.Drawing.Point(288, 28);
+            this.listBoxContatti.Name = "listBoxContatti";
+            this.listBoxContatti.Size = new System.Drawing.Size(326, 212);
+            this.listBoxContatti.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(626, 416);
+            this.Controls.Add(this.listBoxContatti);
+            this.Controls.Add(this.grbContatto);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Rubrica";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.grbContatto.ResumeLayout(false);
+            this.grbContatto.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem modificaContattoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminaContattoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem esciToolStripMenuItem;
+        private System.Windows.Forms.GroupBox grbContatto;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtIndirizzo;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCognome;
+        private System.Windows.Forms.Button btnCreaContatto;
+        private System.Windows.Forms.ListBox listBoxContatti;
+        private System.Windows.Forms.ToolStripMenuItem ricercaContattoToolStripMenuItem;
     }
 }
 
--- Esercizio03 - Rubrica/Form1.cs
+++ Esercizio03 - Rubrica/Form1.cs
@@ -1,20 +1,378 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace Esercizio03___Rubrica
 {
     public partial class Form1 : Form
     {
+        // Percorso del file XML - potrebbe essere passato come parametro o impostato dinamicamente
+        private string GetXmlFilePath()
+        {
+            // Se desideri impostare un percorso fisso puoi scriverlo qui, ad esempio: 
+            // return "C:\\Rubrica\\contatti.xml";
+            // Altrimenti si può usare un percorso dinamico come la directory corrente:
+            return Path.Combine(Application.StartupPath, "contatti.xml");
+        }
+
         public Form1()
         {
             InitializeComponent();
+            CaricaContatti();
         }
+
+        private void CaricaContatti()
+        {
+            try
+            {
+                string xmlFilePath = GetXmlFilePath();
+
+                // Controlla se il file esiste, altrimenti crea un nuovo file XML vuoto
+                if (!File.Exists(xmlFilePath))
+                {
+                    // Se non esiste, crea un file vuoto con la radice "Contatti"
+                    XDocument nuovoDoc = new XDocument(new XElement("Contatti"));
+                    MessageBox.Show("File creato con successo");
+                    nuovoDoc.Save(xmlFilePath);
+                }
+
+                // Carica i contatti esistenti dal file XML
+                XDocument doc = XDocument.Load(xmlFilePath);
+                var contatti = doc.Descendants("Contatto")
+                .Select(x => new
+                {
+                    NomeCompleto = $"{x.Element("Cognome")?.Value} {x.Element("Nome")?.Value}",
+                    Telefono = x.Element("Telefono")?.Value ?? string.Empty,
+                    Email = x.Element("Email")?.Value ?? string.Empty
+                }).ToList();
+
+                // Popola la ListBox con i contatti
+                listBoxContatti.DataSource = contatti;
+                listBoxContatti.DisplayMember = "NomeCompleto";  // Mostra Nome e Cognome
+                listBoxContatti.ValueMember = "Email";  // Mantiene l'email come valore selezionato
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore nel caricamento dei contatti: " + ex.Message);
+            }
+        }
+
+        // Metodo migliorato per la validazione dell'email
+        private bool emailValida(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void esciToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnCreaContatto_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                string xmlFilePath = GetXmlFilePath();
+
+                // Controlla che l'email sia valida
+                if (!emailValida(txtEmail.Text))
+                {
+                    MessageBox.Show("Inserisci una email valida!");
+                    return;
+                }
+
+                // Se il file non esiste, lo crea (verifica extra)
+                if (!File.Exists(xmlFilePath))
+                {
+                    XDocument nuovoDoc = new XDocument(new XElement("Contatti"));
+                    nuovoDoc.Save(xmlFilePath);
+                }
+
+                // Crea un nuovo elemento "Contatto" con i dati delle TextBox
+                XElement nuovoContatto = new XElement("Contatto",
+                    new XElement("Cognome", txtCognome.Text),
+                    new XElement("Nome", txtNome.Text),
+                    new XElement("Indirizzo", txtIndirizzo.Text),
+                    new XElement("Email", txtEmail.Text),
+                    new XElement("Telefono", txtTelefono.Text));
+
+                // Carica il documento XML esistente
+                XDocument doc = XDocument.Load(xmlFilePath);
+
+                // Aggiunge il nuovo contatto al nodo "Contatti"
+                doc.Element("Contatti").Add(nuovoContatto);
+
+                // Salva il file XML con il nuovo contatto
+                doc.Save(xmlFilePath);
+
+                MessageBox.Show("Contatto aggiunto e file salvato correttamente!");
+
+                // Ricarica i contatti per aggiornare la visualizzazione
+                CaricaContatti();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore nell'aggiunta del contatto: " + ex.Message);
+            }
+
+            //svuota i campi dopo aver aggiunto un contatto
+
+            txtCognome.Text = "";
+            txtNome.Text = "";
+            txtIndirizzo.Text = "";
+            txtEmail.Text = "";
+            txtTelefono.Text = "";
+        }
+
+        private void listBoxContatti_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selectedContact = (dynamic)listBoxContatti.SelectedItem;
+            if (selectedContact != null)
+            {
+                // Carica i dati del contatto selezionato nei campi dinamici (che creeremo sotto)
+                CaricaDatiContattoSelezionato(selectedContact);
+            }
+        }
+
+        private TextBox txtModificaEmail, txtModificaTelefono;
+
+        private void modificaContattoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Verifica se ci sono altre GroupBox nella stessa posizione
+            var existingGroupBox = this.Controls.OfType<GroupBox>()
+                .FirstOrDefault(gb => gb.Left == 12 && gb.Top == 250);
+
+            if (existingGroupBox != null)
+            {
+                // Sposta la GroupBox a destra
+                existingGroupBox.Left += existingGroupBox.Width + 80;
+            }
+
+            // Crea il GroupBox per la modifica del contatto
+            GroupBox groupBoxModifica = new GroupBox
+            {
+                Text = "Modifica Contatto",
+                Left = 12,
+                Top = 250,
+                Width = 320,
+                Height = 150
+            };
+
+            // Crea le label e le TextBox per Email e Telefono
+            Label lblEmail = new Label { Text = "Email", Left = 10, Top = 20, AutoSize = true };
+            txtModificaEmail = new TextBox { Left = 100, Top = 20, Width = 200 };
+
+            Label lblTelefono = new Label { Text = "Telefono", Left = 10, Top = 60, AutoSize = true };
+            txtModificaTelefono = new TextBox { Left = 100, Top = 60, Width = 200 };
+
+            // Aggiungi un pulsante di conferma per salvare le modifiche
+            Button btnSalvaModifiche = new Button { Text = "Salva", Left = 100, Top = 100, Width = 100 };
+            btnSalvaModifiche.Click += new EventHandler(SalvaModificheContatto);
+
+            //aggiungere un pulsante che mi permetta
+
+            // Aggiungi i controlli al GroupBox
+            groupBoxModifica.Controls.Add(lblEmail);
+            groupBoxModifica.Controls.Add(txtModificaEmail);
+            groupBoxModifica.Controls.Add(lblTelefono);
+            groupBoxModifica.Controls.Add(txtModificaTelefono);
+            groupBoxModifica.Controls.Add(btnSalvaModifiche);
+
+            // Aggiungi il GroupBox al form
+            this.Controls.Add(groupBoxModifica);
+
+            // Carica i dati del contatto selezionato
+            var selectedContact = (dynamic)listBoxContatti.SelectedItem;
+            if (selectedContact != null)
+            {
+                CaricaDatiContattoSelezionato(selectedContact);
+            }
+        }
+
+
+
+        private void CaricaDatiContattoSelezionato(dynamic contatto)
+        {
+            // Assegna i valori alle TextBox create dinamicamente
+            txtModificaEmail.Text = contatto.Email;
+            txtModificaTelefono.Text = contatto.Telefono;
+        }
+
+        private void eliminaContattoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Verifica se è stato selezionato un contatto nella ListBox
+            var contattoSelezionato = (dynamic)listBoxContatti.SelectedItem;
+            if (contattoSelezionato == null)
+            {
+                MessageBox.Show("Seleziona un contatto da eliminare / Inizia con la creazione di un nuovo contatto.");
+                return;
+            }
+
+            // Mostra un messaggio di conferma con nome completo
+            string messaggio = $"Sei sicuro di voler eliminare il contatto?\n\nNome Completo: {contattoSelezionato.NomeCompleto}";
+            DialogResult risultato = MessageBox.Show(messaggio, "Conferma Eliminazione", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            // Se l'utente conferma, procedi con l'eliminazione
+            if (risultato == DialogResult.Yes)
+            {
+                string xmlFilePath = GetXmlFilePath(); // Assicurati di avere un metodo per ottenere il percorso del file XML
+
+                try
+                {
+                    XDocument doc = XDocument.Load(xmlFilePath);
+
+                    // Trova il contatto da eliminare
+                    var contattoDaEliminare = doc.Descendants("Contatto")
+                        .Where(x => x.Element("Email")?.Value == contattoSelezionato.Email)
+                        .FirstOrDefault();
+
+                    if (contattoDaEliminare != null)
+                    {
+                        // Rimuove il contatto dal documento XML
+                        contattoDaEliminare.Remove();
+                        doc.Save(xmlFilePath);
+
+                        MessageBox.Show("Contatto eliminato con successo!");
+
+                        // Ricarica i contatti per aggiornare la visualizzazione
+                        CaricaContatti();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Contatto non trovato.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Errore durante l'eliminazione: " + ex.Message);
+                }
+            }
+        }
+
+        private GroupBox groupBoxRicerca;
+        private TextBox txtRicerca;
+
+        private void ricercaContattoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Verifica se il GroupBox di ricerca esiste già
+            if (groupBoxRicerca == null)
+            {
+                // Crea il GroupBox per la ricerca
+                groupBoxRicerca = new GroupBox
+                {
+                    Text = "Ricerca Contatto",
+                    Left = 12,
+                    Top = 250,
+                    Width = 250,
+                    Height = 150
+                };
+
+                // Crea le label e le TextBox per la ricerca
+                Label lblRicerca = new Label { Text = "Ricerca", Left = 10, Top = 20, AutoSize = true };
+                txtRicerca = new TextBox { Left = 100, Top = 20, Width = 120 };
+
+                // Aggiungi un pulsante di conferma per avviare la ricerca
+                Button btnRicerca = new Button { Text = "Cerca", Left = 100, Top = 100, Width = 100 };
+                btnRicerca.Click += new EventHandler(CercaContatto);
+
+                // Aggiungi i controlli al GroupBox
+                groupBoxRicerca.Controls.Add(lblRicerca);
+                groupBoxRicerca.Controls.Add(txtRicerca);
+                groupBoxRicerca.Controls.Add(btnRicerca);
+
+                // Aggiungi il GroupBox al form
+                this.Controls.Add(groupBoxRicerca);
+            }
+        }
+
+        private void CercaContatto(object sender, EventArgs e)
+        {
+            string xmlFilePath = GetXmlFilePath();
+
+            try
+            {
+                XDocument doc = XDocument.Load(xmlFilePath);
+
+                // Trova il contatto da cercare tramite il nome, cognome o telefono
+                var contattoDaCercare = doc.Descendants("Contatto")
+                    .Where(x => x.Element("Nome")?.Value == txtRicerca.Text ||
+                                x.Element("Cognome")?.Value == txtRicerca.Text ||
+                                x.Element("Telefono")?.Value == txtRicerca.Text)
+                    .FirstOrDefault();
+
+                if (contattoDaCercare != null)
+                {
+                    // Mostra i dettagli del contatto trovato
+                    string messaggio = $"Contatto trovato:\n\nNome: {contattoDaCercare.Element("Nome")?.Value}\nCognome: {contattoDaCercare.Element("Cognome")?.Value}\nEmail: {contattoDaCercare.Element("Email")?.Value}\nTelefono: {contattoDaCercare.Element("Telefono")?.Value}";
+                    MessageBox.Show(messaggio);
+                }
+                else
+                {
+                    MessageBox.Show("Contatto non trovato.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore durante la ricerca: " + ex.Message);
+            }
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            // apre una nuova finestra dove sono indicate le istruzioni per l'uso del programma
+
+            MessageBox.Show("Benvenuto nella rubrica!\n\nPer aggiungere un contatto, compila tutti i campi e clicca su 'Aggiungi Contatto'.\n\nPer modificare un contatto, selezionalo dalla lista e clicca su 'Modifica Contatto'.\n\nPer eliminare un contatto, selezionalo dalla lista e clicca su 'Elimina Contatto'.\n\nPer cercare un contatto, clicca su 'Cerca Contatto' e inserisci il nome o l'email del contatto da cercare.\n\nBuon utilizzo!", "Istruzioni");
+        }
+
+        private void SalvaModificheContatto(object sender, EventArgs e)
+        {
+            string xmlFilePath = GetXmlFilePath();
+
+            try
+            {
+                XDocument doc = XDocument.Load(xmlFilePath);
+
+                // Trova il contatto da modificare tramite l'email originale o altro identificatore univoco
+                var contattoDaModificare = doc.Descendants("Contatto")
+                    .Where(x => x.Element("Email")?.Value == txtModificaEmail.Text)
+                    .FirstOrDefault();
+
+                if (contattoDaModificare != null)
+                {
+                    // Aggiorna solo i valori dell'email e del telefono
+                    contattoDaModificare.Element("Email").Value = txtModificaEmail.Text;
+                    contattoDaModificare.Element("Telefono").Value = txtModificaTelefono.Text;
+
+                    // Salva il documento XML aggiornato
+                    doc.Save(xmlFilePath);
+
+                    MessageBox.Show("Contatto modificato con successo!");
+
+                    // Ricarica i contatti per aggiornare la visualizzazione
+                    CaricaContatti();
+                }
+                else
+                {
+                    MessageBox.Show("Contatto non trovato.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore durante il salvataggio: " + ex.Message);
+            }
+        }
+
+
     }
 }
